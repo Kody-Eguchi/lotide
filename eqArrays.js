8de8cf89a4cb6ef70d6a2af5907eda699c00187f
Koji Eguchi <koji.eguchi@hotmail.com>
2023-02-10 17:20:58 -0500

    optimized codes

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,31 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
     console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+    return;
   }
+  console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
 };
 
+// const eqArrays = function(arrOne, arrTwo) {
+//   let convertedArrOne = arrOne.toString();
+//   let convertedArrTwo = arrTwo.toString();
+//   return convertedArrOne === convertedArrTwo;
+// };
+
+
 const eqArrays = function(arrOne, arrTwo) {
-  let convertedArrOne = arrOne.toString();
-  let convertedArrTwo = arrTwo.toString();
-  return convertedArrOne === convertedArrTwo;
+
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 3, 3], [1, 4, 3]), false);
