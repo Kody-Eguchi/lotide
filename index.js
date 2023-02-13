edfaf43fd3fb3b13e500a8881c7fcd2607d9fd39
Koji Eguchi <koji.eguchi@hotmail.com>
2023-02-13 18:42:18 -0500

    added eqArrays, eqObjects, findKeyByValue modules

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeyByValue');
 
 module.exports = {
   head,
   tail,
   middle,
+  eqArrays,
+  eqObjects,
+  findKeyByValue,
 };
