e56446bc06afce0ab4e0d26ead8c1653409375fa
Koji Eguchi <koji.eguchi@hotmail.com>
2023-02-13 09:53:38 -0500

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,8 @@
-const eqArrays = function (arrOne, arrTwo) {
-  let convertedArrOne = arrOne.toString();
-  let convertedArrTwo = arrTwo.toString();
-
-  return convertedArrOne === convertedArrTwo;
-};
-
-const assertArrEq = function (eqCheck) {
-  if (eqCheck) {
-    console.log(`✅✅✅Assertion Passed`);
-  } else if (!eqCheck) {
+const assertArrEq = function(eqCheck) {
+  if (!eqCheck) {
     console.log(`🛑🛑🛑Assertion Failed`);
   }
+  console.log(`✅✅✅Assertion Passed`);
 };
 
-assertArrEq(eqArrays(["hello", "goodbye"], ["hello", "goodbye"]));
-assertArrEq(eqArrays(["hello", "goodbye"], ["hola", "goodbye"]));
-assertArrEq(eqArrays([3, 6, 9], [3, 6, 9]));
-assertArrEq(eqArrays([3, 6, 9], [3, 7, 7]));
-assertArrEq(eqArrays(["hello", 55], ["hello", 55]));
-assertArrEq(eqArrays(["hello", 55], ["hello", 77]));
+module.exports = assertArrEq;
