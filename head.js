733e67963121ab761b02e05679ee86b6363d4f41
Koji Eguchi <koji.eguchi@hotmail.com>
2023-02-13 10:09:48 -0500

    fixed a bug for if statement condition

--- head.js
+++ head.js
@@ -1,10 +1,10 @@
 const head = function(arr) {
   const firstElement = arr[0];
-  if (firstElement === undefined || arr.length === 1) {
+  if (firstElement === undefined) {
     return "Only an array contains more than one element is accepted as argument";
-  } else {
-    return firstElement;
   }
+
+  return firstElement;
 };
 
 module.exports = head;
